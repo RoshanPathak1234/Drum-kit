41f1421214d3e502c4d16518522b02a093ce3a8f
RoshanPathak1234 <142977225+RoshanPathak1234@users.noreply.github.com>
2023-09-10 15:20:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+document.querySelectorAll(".drum")[0].addEventListener("click" , function (){
+    let a=new Audio("crash.mp3")
+    a.play()
+})
+document.querySelectorAll(".drum")[1].addEventListener("click" , function (){
+    let a=new Audio("kick-bass.mp3")
+    a.play()
+})
+document.querySelectorAll(".drum")[2].addEventListener("click" , function (){
+    let a=new Audio("snare.mp3")
+    a.play()
+})
+document.querySelectorAll(".drum")[3].addEventListener("click" , function (){
+    let a=new Audio("tom-1.mp3")
+    a.play()
+})
+document.querySelectorAll(".drum")[4].addEventListener("click" , function (){
+    let a=new Audio("tom-2.mp3")
+    a.play()
+})
+document.querySelectorAll(".drum")[5].addEventListener("click" , function (){
+    let a=new Audio("tom-3.mp3")
+    a.play()
+})
+document.querySelectorAll(".drum")[6].addEventListener("click" , function (){
+    let a=new Audio("tom-4.mp3")
+    a.play()
+})
