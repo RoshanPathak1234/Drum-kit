59a7b9a42126c3dc47364307138213c39c6a1eb4
RoshanPathak1234 <142977225+RoshanPathak1234@users.noreply.github.com>
2023-09-29 23:44:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
-document.querySelectorAll(".drum")[0].addEventListener("click" , function (){
-    let a=new Audio("crash.mp3")
-    a.play()
+const len = document.querySelectorAll(".drum").length
+
+for(i=0;i<len;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function()
+    {
+        const button = this.innerHTML;
+        makeSound(button);
+        addAnimation(button);
+    })
+}
+document.addEventListener("keypress" , function(event){
+    makeSound(event.key);
+    addAnimation(event.key);
 })
-document.querySelectorAll(".drum")[1].addEventListener("click" , function (){
-    let a=new Audio("kick-bass.mp3")
-    a.play()
-})
-document.querySelectorAll(".drum")[2].addEventListener("click" , function (){
-    let a=new Audio("snare.mp3")
-    a.play()
-})
-document.querySelectorAll(".drum")[3].addEventListener("click" , function (){
-    let a=new Audio("tom-1.mp3")
-    a.play()
-})
-document.querySelectorAll(".drum")[4].addEventListener("click" , function (){
-    let a=new Audio("tom-2.mp3")
-    a.play()
-})
-document.querySelectorAll(".drum")[5].addEventListener("click" , function (){
-    let a=new Audio("tom-3.mp3")
-    a.play()
-})
-document.querySelectorAll(".drum")[6].addEventListener("click" , function (){
-    let a=new Audio("tom-4.mp3")
-    a.play()
-})
+function makeSound(button)
+{
+    switch(button)
+    {
+        case 'w' || 'w' : 
+            const tom1 = new Audio("tom-1.mp3");
+            tom1.play();
+            break;
+        case 'a' || 'A' : 
+            const tom2 = new Audio("tom-2.mp3");
+            tom2.play();
+            break;
+        case 's' || 'S' : 
+            const tom3 = new Audio("tom-3.mp3");
+            tom3.play();
+            break;
+        case 'd' || 'D' : 
+            const tom4 = new Audio("tom-4.mp3");
+            tom4.play();
+            break;
+        case 'j' || 'J' : 
+            const snare = new Audio("snare.mp3");
+            snare.play();
+            break;
+        case 'k' || 'K' : 
+            const kick = new Audio("crash.mp3");
+            kick.play();
+            break;
+        case 'l' || 'L' : 
+            const kick_bass = new Audio("kick-bass.mp3");
+            kick_bass.play();
+            break;
+        default : breaak;
+    }
+}
+
+function addAnimation(key)
+{
+    const activeButton = document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
